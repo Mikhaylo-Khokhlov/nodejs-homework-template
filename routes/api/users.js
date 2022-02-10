d540c793fe907b55aaf9649b6dc4a4673548ac61
Mikhaylo-Khokhlov <mihailhohlov257@gmail.com>
2022-02-10 21:30:22 +0200

    Feat. Add avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const CreateError = require("http-errors");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User, schemas } = require("../../models/user");
 const authenticate = require("../../middleware/authenticate");
+const upload = require("../../middleware/upload");
 
 const router = express.Router();
 
@@ -39,7 +43,7 @@ router.patch("/", authenticate, async (req, res, next) => {
       { new: true }
     );
     if (resUpdateUserSubscription) {
-      res.status(200).json('Subscription update was successful');
+      res.status(200).json("Subscription update was successful");
     } else {
       throw new CreateError(404, "Not found");
     }
@@ -47,5 +51,40 @@ router.patch("/", authenticate, async (req, res, next) => {
     next(e);
   }
 });
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  async (req, res, next) => {
+    const { _id } = req.user;
+    if (!req.file) {
+      throw new CreateError(400, "Please upload file");
+    }
+    const { path: tempUpload, filename } = req.file;
+    await Jimp.read(tempUpload)
+      .then((item) => {
+        return item.resize(250, 250).write(tempUpload);
+      })
+      .catch((error) => {
+        next(error);
+      });
+    try {
+      const [extention] = filename.split(".").reverse();
+      const newFileName = `${_id}.${extention}`;
+      const resultUpload = path.join(avatarsDir, newFileName);
+      await fs.rename(tempUpload, resultUpload);
+      const avatarURL = path.join("avatars", newFileName);
+      await User.findByIdAndUpdate(_id, { avatarURL });
+      res.json({
+        avatarURL,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
